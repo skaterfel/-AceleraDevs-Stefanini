80b5f679b12440536ff33029a10c2adddd68fbeb
Administrator <administrator@iMac-de-Administrator.local>
2022-11-09 12:10:13 -0300

    Rotas de exclusão criada

--- src/main/java/com/stefanini/aceleraDevs/controller/AlunoController.java
+++ src/main/java/com/stefanini/aceleraDevs/controller/AlunoController.java
@@ -1,9 +1,13 @@
 package com.stefanini.aceleraDevs.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +17,7 @@ import com.stefanini.aceleraDevs.dto.AlunoDTO;
 import com.stefanini.aceleraDevs.exception.TurmaNotFoundException;
 import com.stefanini.aceleraDevs.mapper.AlunoDTOService;
 import com.stefanini.aceleraDevs.model.Aluno;
+import com.stefanini.aceleraDevs.model.Turma;
 import com.stefanini.aceleraDevs.repository.AlunoRepository;
 import com.stefanini.aceleraDevs.service.AlunoService;
 
@@ -57,6 +62,15 @@ public class AlunoController {
         List<Aluno> alunos = alunoRepository.findAll();
         return AlunoDTO.converter(alunos);
 
+    }
+    @DeleteMapping("/alunos/{id}")
+    public ResponseEntity<?> remove(@PathVariable Long id) {
+        Optional<Aluno> optional = alunoRepository.findById(id);
+        if (optional.isPresent()) {
+            alunoRepository.deleteById(id);
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
     }
 
 }
